e91dd1f58c77e75e2d05ad519ec10598a965d2ba
mickey_bobby <fukudadisneyfun@gmail.com>
2024-01-05 02:35:57 +0900

    LK法を2パターン用意

--- 完成版/lin-kernighan_2.py
+++ 完成版/lin-kernighan_2.py
@@ -1,18 +1,11 @@
 from week6_3d import cal_dist, calculate_total_distance, read_tsp_file, tsp_approximation, minimum_spanning_tree, read_atsp_file
 import time
 
-# from python_tsp.heuristics import solve_tsp_lin_kernighan
+from python_tsp.heuristics import solve_tsp_lin_kernighan
 # from python_tsp.heuristics import solve_tsp_simulated_annealing
-from tsp_LK import tsp_LK
-# import python_tsp
 import numpy as np
 
-# xopt, fopt = solve_tsp_lin_kernighan(
-#     distance_matrix: np.ndarray,
-#     x0: Optional[List[int]] = None,
-#     log_file: Optional[str] = None,
-#     verbose: bool = False,
-# )
+
 
 
 
@@ -20,6 +13,7 @@ if __name__ == '__main__':
     start_time = time.time()
     # file_path = 'ATSPlib/ft70.atsp'
     # file_path = 'TSPlib/eil101.tsp'
+    # file_path = 'TSPlib/pr264.tsp'
     file_path = 'TSPlib/d1655.tsp'
     
     if file_path[-4:] == 'atsp':
@@ -30,14 +24,18 @@ if __name__ == '__main__':
     startpoint = 0
     N = len(dist)
 
-    two_ap_tour = np.random.permutation(N)
+    two_ap_tour = list(np.random.permutation(N))
+    print(len(two_ap_tour),N)
     two_ap_tour_length = calculate_total_distance(dist, two_ap_tour)
     # spanning_tree = minimum_spanning_tree(dist, startpoint, N) #最小全域木を生成
     # two_ap_tour = tsp_approximation(spanning_tree, startpoint)
     # two_ap_tour_length = calculate_total_distance(dist, two_ap_tour)
-    print(f"{N,len(two_ap_tour)},2近似アルゴリズムの経路長は{two_ap_tour_length}")
+    print(f"2近似アルゴリズムの経路長は{two_ap_tour_length}")
+    if two_ap_tour[-1] == two_ap_tour[0]:
+        two_ap_tour = two_ap_tour[:-1]
+    xopt, fopt = solve_tsp_lin_kernighan(dist,two_ap_tour,None,False)
     
-    LK_tour, LK_tour_length = tsp_LK(dist, np.array(two_ap_tour), two_ap_tour_length)
-    print(f"本物の経路長:{calculate_total_distance(dist, LK_tour)},経路長は{LK_tour_length}")
+    # LK_tour, LK_tour_length = tsp_LK(dist, np.array(two_ap_tour), two_ap_tour_length)
+    print(f"本物の経路長:{calculate_total_distance(dist, xopt)},経路長は{fopt}")
     end_time = time.time()
     print(f"計算時間は{end_time-start_time:.5f}秒")
