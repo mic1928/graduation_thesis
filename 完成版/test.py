84203ed77de25ff2c6d72ca118ce00f0bc6c1eb7
mickey_bobby <fukudadisneyfun@gmail.com>
2024-01-15 04:17:01 +0900

    グラフの描画コードをtest.pyに追加

--- 完成版/test.py
+++ 完成版/test.py
@@ -376,64 +376,108 @@ plt.grid(True)
 plt.show()
 """
 
-# file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/eil101_tsp.txt"
-# file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/pr264_tsp.txt"
-# file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/lin318_tsp.txt"
+# # file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/eil101_tsp.txt"
+# # file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/pr264_tsp.txt"
+# # file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/lin318_tsp.txt"
 # file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/p654_tsp.txt"
-# file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/d1655_tsp.txt"
-file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/rbg443_atsp.txt"
+# # file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/d1655_tsp.txt"
+# # file_path = "/Users/tomo.f/Desktop/卒論/完成版/実行結果/提案手法/rbg443_atsp.txt"
 
-try:
-    with open(file_path, 'r') as file:
-        # ファイルから全ての行を読み込んでリストに格納
-        content = file.readlines()
+# try:
+#     with open(file_path, 'r') as file:
+#         # ファイルから全ての行を読み込んでリストに格納
+#         content = file.readlines()
 
-    length_list = []
-    time_list = []
-    for line in content:
-        if line[0] != "i":
-            continue
-        else:
-            length_list.append(float(line.split('length:')[1].split(' ')[0]))
-            time_list.append(float(line.split('lap_time:')[1]))
+#     length_list = []
+#     time_list = []
+#     for line in content:
+#         if line[0] != "i":
+#             continue
+#         else:
+#             length_list.append(float(line.split('length:')[1].split(' ')[0]))
+#             time_list.append(float(line.split('lap_time:')[1]))
 
-    length_list = length_list[:200]
-    time_list = time_list[:200]
+#     length_list = length_list[-200:]
+#     time_list = time_list[-200:]
             
 
 
-    print(length_list)
-    print(len(length_list))
-    print(time_list)
-    print(len(time_list))
+#     print(length_list)
+#     print(len(length_list))
+#     print(time_list)
+#     print(len(time_list))
 
-except FileNotFoundError:
-    print(f"指定されたファイル '{file_path}' が見つかりませんでした。")
-except Exception as e:
-    print(f"エラーが発生しました: {e}")
+# except FileNotFoundError:
+#     print(f"指定されたファイル '{file_path}' が見つかりませんでした。")
+# except Exception as e:
+#     print(f"エラーが発生しました: {e}")
 
+# import matplotlib.pyplot as plt
+
+# def plot_line_chart(x_values, y_values):
+#     """
+#     x_values: x軸の値を格納したリスト
+#     y_values: y軸の値を格納したリスト
+#     x_label: x軸のラベル
+#     y_label: y軸のラベル
+#     title: グラフのタイトル
+#     """
+#     plt.plot(x_values, y_values, linestyle='-', label='Relaxation')
+#     # plt.axhline(y=629, color='r', linestyle='--', label='Exact Solution')
+#     # plt.axhline(y=49135, color='r', linestyle='--', label='Exact Solution')
+#     # plt.axhline(y=42029, color='r', linestyle='--', label='Exact Solution')
+#     plt.axhline(y=34643, color='r', linestyle='--', label='Exact Solution')
+#     # plt.axhline(y=62128, color='r', linestyle='--', label='Exact Solution')
+#     # plt.axhline(y=2720, color='r', linestyle='--', label='Exact Solution')
+#     plt.xlabel("Execution Time (seconds)")
+#     plt.ylabel("Tour Length")
+#     # plt.title(title)
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
+
+# plot_line_chart(time_list, length_list)
+
+
+import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_line_chart(x_values, y_values):
-    """
-    x_values: x軸の値を格納したリスト
-    y_values: y軸の値を格納したリスト
-    x_label: x軸のラベル
-    y_label: y軸のラベル
-    title: グラフのタイトル
-    """
-    plt.plot(x_values, y_values, linestyle='-', label='Relaxation')
-    # plt.axhline(y=629, color='r', linestyle='--', label='Exact Solution')
-    # plt.axhline(y=49135, color='r', linestyle='--', label='Exact Solution')
-    # plt.axhline(y=42029, color='r', linestyle='--', label='Exact Solution')
-    # plt.axhline(y=34643, color='r', linestyle='--', label='Exact Solution')
-    # plt.axhline(y=62128, color='r', linestyle='--', label='Exact Solution')
-    plt.axhline(y=2720, color='r', linestyle='--', label='Exact Solution')
-    plt.xlabel("Execution Time (seconds)")
-    plt.ylabel("Tour Length")
-    # plt.title(title)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# 入力リスト
+# x_data = [101, 264, 318, 654, 1655]
+# y_datas = [[0.004 , 0.03 , 0.04 , 0.2 , 1.0], [0.008 , 0.05 , 0.07 , 0.3 , 2.0],[4.2 , 47.9 , 75.3 , 603.4 , 5264.4],[0.02 , 0.2 , 0.9 , 8.1 , 150.4],[0.2 , 2.9 , 4.5 , 40.3 , 3853.3],[984.8 , 1918.2 , 2435.3 , 7926.9 , 29032.1]]
+# labels = ["Nearest Neighbor      ","2-app                        ","Simulated Annealing","2-opt                         ","Lin-Kernighan            ","Relaxation                 "]
 
-plot_line_chart(time_list, length_list)
+x_data = [39, 70, 323, 358, 443]
+y_datas = [[0.001 , 0.002 , 0.03 , 0.05 , 0.07], [0.002 , 0.005 , 0.05 , 0.07 , 0.1],[0.5 , 1.8 , 47.7 , 62.7 , 113.7],[0.03 , 0.1 , 17.8 , 25.4 , 50.7],[386.3 , 804.6 , 1050.6 , 1259.4 , 1467.7]]
+labels = ["Nearest Neighbor      ","2-app                        ","Simulated Annealing","2-opt                         ","Relaxation                 "]
+
+# データをnumpy配列に変換し、両対数変換を行う
+log_x = np.log10(x_data)
+log_ys = np.array([np.log10(y_data) for y_data in y_datas])
+
+# 線形回帰を行う
+coeffss = np.array([np.polyfit(log_x, log_y, 1) for log_y in log_ys])
+
+# 近似直線の式を作成する
+equations = [f"y = {10 ** coeffs[1]:.10f}x^{coeffs[0]:.6f}" for coeffs in coeffss]
+
+
+x_data_extention = [x_data[0], x_data[-1]*6]
+# 元のデータと近似直線をプロットする
+plt.figure(figsize=(8, 8))
+for i, y_data in enumerate(y_datas):
+    plt.scatter(x_data, y_data, marker='o')
+    plt.plot(x_data_extention, 10 ** coeffss[i][1] * np.array(x_data_extention) ** coeffss[i][0], label=f'{labels[i]:<20} Fitted line:{equations[i]}')
+
+# plt.plot(x_data, 10 ** coeffs[1] * np.array(x_data) ** coeffs[0], label=f'Fitted line:{equation}')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Number of Cities')
+plt.ylabel('Execution Time (seconds)')
+# plt.title('2D log-log plot with fitted line')
+# plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+plt.legend(loc='upper center', bbox_to_anchor=(.5, -.10))
+plt.tight_layout()
+plt.grid(True)
+plt.show()
+
